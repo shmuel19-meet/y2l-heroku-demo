c355a3b69dbcea64e82e7720006b1f0b4a2cec89
Shmuel19-meet <shmuel19-meet@meet.mit.edu>
2018-11-22 18:12:13 +0200

    A test for the new website

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'WORKS'
+    return render_template('homepage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
